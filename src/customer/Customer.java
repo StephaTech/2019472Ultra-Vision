6177c95d320e0f7af292aa4aadf2b090760c0631
StephaTech <stephanysouza.direito@gmail.com>
2020-04-13 16:31:56 +0100

    classes
    creating

--- src/customer/Customer.java
+++ src/customer/Customer.java
@@ -1,5 +1,13 @@
 package customer;
 
 public class Customer {
+	private String ID;
+	private String fisrtName;
+	private String lastName;
+	private String Address;
+	private String Membership;
+	private String Mobile;
+	int Points;
 
+	public Customer{}
 }
--- src/titles/Titles.java
+++ src/titles/Titles.java
@@ -1,5 +1,7 @@
 package titles;
 
 public class Titles {
-
+	private String Type;
+	private String Name;
+	private String Year;
 }
