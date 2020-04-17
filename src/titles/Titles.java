42bb6f09c54b89dfa084690dd11e7bf7051c7294
StephaTech <stephanysouza.direito@gmail.com>
2020-04-17 23:58:04 +0100

    class titles
    defining some attributes and behaviour

--- src/titles/Titles.java
+++ src/titles/Titles.java
@@ -1,7 +1,11 @@
 package titles;
 
 public class Titles {
-	private String Type;
-	private String Name;
-	private String Year;
+	private String titleType;//titles in the shop are broken down into types: selected
+	private String titleName;//name of the title up to me
+	private String genre;
+	private String yearOfRelease;
+	private String director;
+	private String band;
+	private String format;//the media formats include CD,DVD, and Blu-Ray
 }
