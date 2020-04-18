613e8b979dd5ef3001d0b80365287658032ce282
StephaTech <stephanysouza.direito@gmail.com>
2020-04-18 23:25:05 +0100

    class title and interface
    keep working and adding stuff.

--- src/titles/Titles.java
+++ src/titles/Titles.java
@@ -4,7 +4,7 @@ public class Titles {
 	private String titleType;//titles in the shop are broken down into types: selected
 	private String titleName;//name of the title up to me
 	private String genre;//specific requirement CA and relevant to the nature
-	private String yearOfRelease;//specific requirement CA and relevant to the nature
+	private int yearOfRelease;//specific requirement CA and relevant to the nature
 	private String director;//specific requirement CA and relevant to the nature
 	private String band;//specific requirement CA and relevant to the nature
 	private String format;//the media formats include CD,DVD, and Blu-Ray
--- src/titles/TitlesInterface.java
+++ src/titles/TitlesInterface.java
@@ -1,7 +1,7 @@
 package titles;
 
-public interface TitlesInterface {
-	//ENUM OR INTERFACE?????
+public interface TitlesInterface {	//ENUM OR INTERFACE?????
+
 	//below is the media formats include CCD,DVD and BlueRay
 	public String format_CD = "CD";
 	public String format_BlueRay = "BLUERAY";
@@ -9,9 +9,11 @@ public interface TitlesInterface {
 	
 	public void Format();//is the media title formats specified as a requirement on the CA
 	
-	public void addTitle(String titleType, String titleName, String genre, String yearOfRelease, String director, String band, String Format );
+	//below is a method
+	public void addTitle(String titleType, String titleName, String genre, int yearOfRelease, String director, String band, String Format );
 	
-	public void updateTitle(int id,String titleType, String titleName, String genre, String yearOfRelease, String director, String band, String Format );
+	//below is a method
+	public void updateTitle(int id,String titleType, String titleName, String genre, int yearOfRelease, String director, String band, String Format );
 
 	
 
